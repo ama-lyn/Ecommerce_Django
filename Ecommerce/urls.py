9ce0e49039fa022a7141f50712e42db5cb9b75e3
Gwendolyn <wilkistagwendolyn@gmail.com>
2024-07-04 14:08:52 +0300

    Ecommerce layout

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'Ecommerce'
+
 urlpatterns = [
-    path('products_list/', views.product_list, name='product_list')
-]
+    path('', views.home, name='home'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('customers/', views.customer_list, name='customer_list'),
+    path('customers/<int:pk>', views.customer_detail, name='customer_detail'),
+    
+]        
--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import get_object_or_404, render
+from .models import Product, Customer
 
 
 # Create your views here.
-
+def home(request):
+    return render(request, 'Ecommerce/home.html')
 
 def product_list(request):
     products = Product.objects.all()
@@ -16,6 +17,24 @@ def product_list(request):
     return render(request, 'Ecommerce/product_list.html', context)
 
 
-def product_details(request):
-    pass
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+        }
+    return render(request, 'Ecommerce/product_detail.html', context)
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    context = {
+        'customers': customers
+    }
+    return render(request,'Ecommerce/customer_list.html', context)
+
+def customer_detail(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    context = {
+        'customer': customer
+    }
+    return render(request,'Ecommerce/customer_detail.html', context)
  
